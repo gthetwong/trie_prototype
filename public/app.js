3991c3966c5ddb3e9ec9ef1c844110ff568d120b
gthetwong <gthetwong@gmail.com>
2014-03-08 17:22:43 -0800

    triejs is working and renders results. should have committed more

--- public/app.js
+++ public/app.js
@@ -10,12 +10,46 @@ window.App = {
     App.autocompleter = new Autocompleter();
     var ws = new WebSocket('ws://' + window.location.host + window.location.pathname);
     ws.onmessage = function(m) { 
-      autocompleter.add(m.data); 
+      App.autocompleter.add(m.data); 
     };
 
   }
 };
 $(document).ready(function(){
   App.initialize();
+});
+
+
+
+
+App.Routers.Main = Backbone.Router.extend({
+  routes: {"": "index"},
+  index: function(){
+    var view = new App.Views.Index();
+    $("#container").append(view.render().el);
+  }
+});
+
+
+
+
+App.Views.Index = Backbone.View.extend({
+  id: "search_temp", 
+  events: {"keyup #search_bar": "search_fnc"},
+  tagName: "span",
+  template: function(){ return "<form> <input id=\"search_bar\" type=\"text\"> Search field</input></form>";},
+
+  render: function(){
+    $(this.el).html(this.template());
+    // alert("hellowwwworld");
+    return this;
+  },
+  search_fnc: function(){
+        var word = $("#search_bar").val();
+        var results = App.autocompleter.complete(word);
+        // $.each(results, function(index, value){
+          $("#titles").html(results);
+        // });
+  }
 });
 
--- public/trie.js
+++ public/trie.js
@@ -3,19 +3,21 @@ Trie = function(){
 };
 
 Trie.prototype.learn = function(word, index){
-  // This function should add the given word,
-  // starting from the given index,
-  // to this Trie.
+  var index = index || 0;
+  var word = word;
+  var char = word[index];
 
-  // It will be recursive.  It will tell
-  // the correct child of this Trie to learn the word
-  // starting from a later index.
+  if (this.characters[char]){
+    this.characters[char].learn(word, index+1);
+  } else {
+    if (index === word.length){
+      this.isWord = true;
+    } else {
+      this.characters[char] = new Trie();
+      this.characters[char].learn(word,index+1);
+    }
+  }
 
-  // Consider what the learn function should do
-  // when it reaches the end of the word?
-  // A word does not necessarily end at a leaf.
-  // You must mark nodes which are the ends of words,
-  // so that the words can be reconstructed later.
 };
 
 Trie.prototype.getWords = function(words, currentWord){
@@ -23,17 +25,62 @@ Trie.prototype.getWords = function(words, currentWord){
   // contained in this Trie.
   // it will use currentWord as a prefix,
   // since a Trie doesn't know about its parents.
+words = words || [];
+currentWord= currentWord || "";
+
+if (this.isWord){
+  words.push(currentWord);
+}
+
+for (var char in this.characters) {
+  var newWord = currentWord + char;
+  if (this.characters[char].characters){
+    this.characters[char].getWords(words, newWord);
+      }
+    }
+  return words;
 };
+
+
+
 
 Trie.prototype.find = function(word, index){
   // This function will return the node in the trie
   // which corresponds to the end of the passed in word.
-
+  var word = word;
+  var index = index || 0;
+  var node = word[index];
+  if (this.characters[node]){
+     return this.characters[node].find (word, index+1);
+  } else if(index === word.length){
+    // console.log(this);
+    // console.log(this.characters);
+   return this; 
+  } else {
+    return false;
+  }
   // Be sure to consider what happens if the word is not in this Trie.
 };
 
 Trie.prototype.autoComplete = function(prefix){
+  // var prefix = prefix.toLowerCase();
+  var node = this.find(prefix);
+  if (!node) {return [];}
+  var result = node.getWords([], prefix);
+  console.log(result);
+  return node.getWords([], prefix); 
   // This function will return all completions 
   // for a given prefix.
   // It should use find and getWords.
 };
+
+
+root = new Trie();
+root.learn("big");
+root.learn("bad");
+root.learn("bigger");
+root.learn("beg");
+root.learn("beginner");
+root.learn("begins");
+root.learn("bald");
+root.learn("balding");
